4bb26dbb1b8f33f3b36eb2a37a730e6faec85c41
Roynoone <woanayr@gmail.com>
2018-11-10 13:38:54 +0100

    app.js updated xd

--- Dochotomie/app.js
+++ Dochotomie/app.js
@@ -1,11 +1,12 @@
 const math = require('mathjs')
 const parser = math.parser();
 
-parser.eval('f(x) = x ^ 3 + x - 1');
+parser.eval('f(x) = sin(4 x ^ 4 + 3 x + 2)');
 
 var a = 0
-var b = 1
-var alpha = parser.eval('10 ^ - 4');
+var b = 0.5
+var alpha = parser.eval('10 ^ - 3');
+ex = 3
 var m = 5
 var sol
 var temp;
@@ -25,5 +26,5 @@ while( b - a > alpha){
   }
 }
 
-console.log("L'encadrement de la solution X est : "+a+" < X < "+b);
-console.log(parser.eval('f(0.68231201171875)'))
+console.log("L'encadrement de la solution X est : "+a+" < X < "+b+"\n\n");
+console.log("La solution à "+alpha+" près est : "+parseFloat(m).toFixed(ex));
